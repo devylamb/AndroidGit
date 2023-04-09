bd34422c4f41638fc2fee45065fa6483c1fef600
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2023-04-09 11:39:54 +0530

    updated

--- demo.sh
+++ demo.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-cd /data/data/com.termux/files/home/storage/shared/Obsidian-notes
+SHARED='/data/data/com.termux/files/home/storage/shared/'
 
-git pull
-git add .
-git commit -m "updated"
-git push origin master
+cd $SHARED
+
+rm -rf Obsidian-notes
+
+git clone git@github.com:AvishekShaw/Obsidian-notes.git
+
+cd Obsidian-notes
+
+
+# git pull
+# git add .
+# git commit -m "updated"
+# git push origin master
