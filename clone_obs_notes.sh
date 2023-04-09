99b280ef1fcc5b8d3c6ed58c639395321bfafca3
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2023-04-09 12:15:56 +0530

    updated

--- clone_obs_notes.sh
+++ clone_obs_notes.sh
@@ -1,5 +1,6 @@
 
-source $HOME/env_var
+OBS_REPO=Obsidian-notes
+SHARED='/data/data/com.termux/files/home/storage/shared/'
 cd $SHARED
 rm -rf Obsidian-notes
 git clone git@github.com:AvishekShaw/Obsidian-notes.git
