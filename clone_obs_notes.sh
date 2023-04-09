3c4a6ef37562ed1e0fb3a4eaa9e5961e34ae79ed
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2023-04-09 12:00:56 +0530

    updated

---
+++ clone_obs_notes.sh
@@ -0,0 +1,5 @@
+
+source $HOME/env_var
+cd $SHARED
+rm -rf Obsidian-notes
+git clone git@github.com:AvishekShaw/Obsidian-notes.git
--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
-source $HOME/repo.conf
-cd $HOME/storage/shared/$GH_REPO
+source $HOME/evn_var
+cd $SHARED/$OBS_REPO
+
 git add .
 git commit -m "android on $(date)"
+
 git push
 cd $HOME
 bash -c "read -t 3 -n 1"
