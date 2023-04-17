1565bbcca928ad544e73559f3d1b91eea8908edf
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2023-04-17 21:42:58 +0530

    updated

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt install curl
+apt install zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
+chsh -s /bin/zsh
