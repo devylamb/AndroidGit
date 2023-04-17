fcd0a437b45f4a86576275379558909b8996aed7
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2023-04-17 21:58:25 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 apt install curl
 apt install zsh
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
