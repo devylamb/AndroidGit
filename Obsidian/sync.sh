50ca824a5083a5113e145812587c2c6b2f40f7c4
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2023-04-30 18:24:06 +0530

    handle files with space

--- Obsidian/sync.sh
+++ Obsidian/sync.sh
@@ -3,7 +3,10 @@ SHARED='/data/data/com.termux/files/home/storage/shared/'
 
 cd $SHARED/$OBS_REPO
 
-git add **/*.md
+
+find . -type f -name "*.md" -print0 | xargs -0 git add
+git ls-files --deleted -z | xargs -0 git add
+
 git commit -m "android on $(date)"
 git pull origin master
 git push origin master
