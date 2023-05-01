cdb632a70f231404565dd5186bc418d3e357a347
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2023-05-01 16:58:17 +0530

    remove repetitive alias

--- Obsidian/clone_obs_notes.sh
+++ Obsidian/clone_obs_notes.sh
@@ -1,6 +1,5 @@
 
-OBS_REPO=Obsidian-notes
-SHARED='/data/data/com.termux/files/home/storage/shared/'
+#!/bin/bash
 cd $SHARED
 rm -rf Obsidian-notes
-git clone git@github.com:AvishekShaw/Obsidian-notes.git
+clone Obsidian-notes
