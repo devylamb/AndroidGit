31c4844b4920b9fadce72f48ed13b9f78326dbc8
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2023-05-01 16:57:48 +0530

    rename

--- Obsidian/nsync.sh
+++ Obsidian/nsync.sh
@@ -1,8 +1,6 @@
-OBS_REPO=Obsidian-notes
-SHARED='/data/data/com.termux/files/home/storage/shared/'
+#!/bin/bash
 
 cd $SHARED/$OBS_REPO
-
 
 find . -type f -name "*.md" -print0 | xargs -0 git add
 git ls-files --deleted -z | xargs -0 git add
