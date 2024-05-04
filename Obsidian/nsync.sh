e2bd15cee6bde288afc1a194030fd5b197a113f2
AvishekShaw <47551320+AvishekShaw@users.noreply.github.com>
2024-05-04 15:15:39 +0530

    updated android

--- Obsidian/nsync.sh
+++ Obsidian/nsync.sh
@@ -2,9 +2,7 @@
 
 cd $SHARED/$OBS_REPO
 
-#find . -type f -name "*.md" -print0 | xargs -0 git add
-
-find . -type f \( -name "*.md" -o -name "*.png" \) -print0 | xargs -0 git addgit ls-files --deleted -z | xargs -0 git add
+find . -type f \( -name "*.md" -o -name "*.png"  -o -name "*.pdf" \) -print0 | xargs -0 git addgit ls-files --deleted -z | xargs -0 git add
 
 git commit -m "android on $(date)"
 git pull origin master
