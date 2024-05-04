4dfda1cb94bdf389cc6e7ace9affa90257f06f59
devy_lamb <l.devyani12@gmail.com>
2024-05-04 15:32:25 +0530

    updated android

--- Obsidian/nsync.sh
+++ Obsidian/nsync.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-SHARED="data/data/com.termux/files/home/storage/shared"
+SHARED="/data/data/com.termux/files/home/storage/shared"
 cd $SHARED/Obsidian-notes
 
-find . -type f \( -name "*.md" -o -name "*.png"  -o -name "*.pdf" \) -print0 | xargs -0 git addgit ls-files --deleted -z | xargs -0 git add
+find . -type f \( -name "*.md" -o -name "*.png"  -o -name "*.pdf" \) -print0 | xargs -0 git add 
+git ls-files --deleted -z | xargs -0 git add
 
 git commit -m "android on $(date)"
-git pull origin master
-git push origin master
+git pull origin main
+git push origin main
